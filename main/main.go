b8e829f0525aba782809310304edc9bf34e8440d
Ivan <lavr.i-n@mail.ru>
2023-01-06 17:50:55 +0300

    done

--- main/main.go
+++ main/main.go
@@ -6,26 +6,51 @@ import (
 )
 
 func main() {
-	var a CLIresps.Editor
-	contact := CLIresps.Contact{}
-	a = &contact
+	for {
+		var (
+			choice string
+			manager CLIresps.Editor
+		)
+		contact := CLIresps.Contact{}
+		manager = &contact
 
-
-
-	a.WriteContact("Ivan", "112343", "gg", "1")
-	a.WriteContact("Vasya", "243213", "wp", "2")
-	a.WriteContact("Asya", "666323", "lol", "3")
-	a.WriteContact("Van", "666313", "lmao", "4")
-	a.WriteContact("Nastya", "666353", "rofl", "5")
-	a.WriteContact("Gosha", "666383", "l", "6")
-	a.ShowAllContacts()
-	a.DeleteContact("2")
-	a.WriteContact("Vasya", "243213", "wp", "2")
-	a.ShowContactInfo("3")
-	fmt.Println()
-	fmt.Println()
-	a.ShowAllContacts()
-	a.WriteContact("Vasya", "243213", "wp", "16")
-	a.DeleteContact("2")
-	a.ShowAllContacts()
+		fmt.Println("_____________________________________________________________________________________________|_________________________________________")
+		fmt.Println("0 - Exit | 1 - Show all | 2 - Show by ID | 3 - Create a contact | 4 - Delete a contact by ID | please use ONE (1) word for a comment...")
+		fmt.Println("`````````````````````````````````````````````````````````````````````````````````````````````|`````````````````````````````````````````")
+		fmt.Scan(&choice)
+		if choice == "0" {
+			break
+		} else {
+			switch choice {
+			case "1":
+				manager.ShowAllContacts()
+			case "2":
+				var id string
+				fmt.Println("Please, type an ID:")
+				fmt.Scan(&id)
+				manager.ShowContactInfo(id)
+				fmt.Println()
+			case "3":
+				var (
+					name string
+					number string
+					comment string
+					id string
+				)
+				fmt.Println("Please, type Name, number, comment, ID:")
+				fmt.Scan(&name, &number, &comment, &id)
+				manager.WriteContact(name, number, comment, id)
+			case "4":
+				var id string
+				fmt.Println("Please, type an ID:")
+				fmt.Scan(&id)
+				manager.DeleteContact(id)
+				fmt.Println()
+			default:
+				fmt.Println("/^\\                        /^\\")
+				fmt.Println(" | please type number (0-4) |")
+				fmt.Println(" |                          |")
+			}
+		}
+	}
 }
